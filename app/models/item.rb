f5e701b6cf0235532312e39d7ad555c0bd68e8c4
Cata Vergara <mcvergara1@miuandes.cl>
2023-05-09 19:02:14 -0400

    Adding references

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,7 @@
 class Item < ApplicationRecord
+    belongs_to :restaurant
 
+    
     validates :name, presence: true
     validates :price, numericality: { greater_than: 0 }
     validates :callories, numericality: { greater_than: 0 }
