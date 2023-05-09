052929ae08068ae59dce813c0be293960c004538
Cata Vergara <mcvergara1@miuandes.cl>
2023-05-09 16:49:44 -0400

    Index

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  resources :restaurants
-  resources :items
-  resources :dishes
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "static_pages#home"
+  resources :restaurants
+  resources :items
+  resources :dishes
 end
